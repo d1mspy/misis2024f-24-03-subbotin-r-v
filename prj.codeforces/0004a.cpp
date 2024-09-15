424276f036f887c492c94df9ea498a4461022bb5
d1mspy <rom.dog20@gmail.com>
2024-09-15 22:47:14 +0300

    Update 0004a.cpp

--- prj.codeforces/0004a.cpp
+++ prj.codeforces/0004a.cpp
@@ -1,1 +1,12 @@
+// 4A Арбуз
+#include <iostream>
 
+int main(){
+int m = 0;
+std::cin >> m;
+if ((0 == m % 2) && (m != 2)) {
+    std::cout << "YES";
+    } else {
+        std::cout << "NO";
+    }
+}
