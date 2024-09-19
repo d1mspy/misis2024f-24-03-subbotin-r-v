516e1fe36cd24320d30c175c3d19d579b9a019cc
d1mspy <rom.dog20@gmail.com>
2024-09-19 15:03:57 +0300

    Update 0978a.cpp

--- prj.codeforces/0978a.cpp
+++ prj.codeforces/0978a.cpp
@@ -6,19 +6,24 @@ int main() {
 	int n;
 	std::vector<int> data, numbers(10000, 0);
 	std::cin >> n;
+
 	for (int i = 0; i < n; ++i) {
 		int a;
 		std::cin >> a;
 		data.push_back(a);
 	}
+	int s = data.size();
 	for (int i = data.size() - 1; i >= 0; --i) {
 		numbers[data[i]] += 1;
 		if (numbers[data[i]] > 1) {
-			data.erase(data.begin() + i);
+			data[i] = -1;
+			s--;
 		}
 	}
-	std::cout << data.size() << "\n";
-	for (int elem : data) {
-		std::cout << elem << " ";
+	std::cout << s << "\n";
+	for (int i = 0; i < data.size(); ++i) {
+		if (data[i] != -1) {
+			std::cout << data[i] << " ";
+		}
 	}
 }
