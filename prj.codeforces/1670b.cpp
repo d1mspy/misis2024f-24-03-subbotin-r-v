a765e8464f56cb9c84e40abcb9176d8db6c0c3b9
d1mspy <rom.dog20@gmail.com>
2024-10-07 00:02:47 +0300

    Create 1670b.cpp

---
+++ prj.codeforces/1670b.cpp
@@ -0,0 +1,49 @@
+// 1670B - общажные войны
+#include <iostream>
+#include <string>
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	int t = 0; std::cin >> t;
+	while (t--) {
+		int n = 0; std::cin >> n;
+		std::string s = ""; std::cin >> s;
+		int k = 0; std::cin >> k;
+		std::string unic = "";
+		for (int i = 0; i < k; ++i) {
+			char c = ' '; std::cin >> c;
+			unic += c;
+		}
+		std::string b = "";
+		bool flag = false;
+		for (int i = 0; i < n; ++i) {
+			int cnt = 1;
+			flag = false;
+			for (int j = 0; j < k; ++j) {
+				if (s[i] == unic[j]) {
+					flag = true;
+					break;
+					}
+				}
+			if (flag == true) {
+				b += '1';
+			} else {
+				b += '0';
+			}
+		}
+		int mx = 0;
+		for (int i = 0; i < n; ++i) {
+			if (b[i] == '1' || i == 0) {
+				for (int j = i+1; j < n; ++j) {
+					if (b[j] == '1') {
+						if (j - i > mx) {
+							mx = j - i;
+						}
+						break;
+					}
+				}
+			}
+		}
+		std::cout << mx << '\n';
+	}
+}
