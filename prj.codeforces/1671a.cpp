0e9d79738ee3ea150cbcc7dc1a1c47c1be8192f2
d1mspy <rom.dog20@gmail.com>
2024-10-31 15:25:34 +0300

    Create 1671a.cpp

---
+++ prj.codeforces/1671a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int t = 0;
+	std::cin >> t;
+	while (t--) {
+		std::string s = "";
+		std::cin >> s;
+		if (s.size() < 2 || s.substr(0, 1) == "ab" || s.substr(0, 1) == "ba" || s.substr(s.size()-2, s.size()-1) == "ab" || s.substr(s.size()-2, s.size()-2) == "ba") {
+			std::cout << "NO" << '\n';
+		} else {
+			bool flag = true;
+			for (int i = 0; i < s.size()-2; ++i) {
+				if (s.substr(i, i+2) == "aba" || s.substr(i, i+2) == "bab") {
+					flag = false;
+					std::cout << "NO" << '\n';
+					break;
+				}
+			}
+			if (flag == true) {
+				std::cout << "YES" << '\n';
+			}
+		}
+	}
+}
